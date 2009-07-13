59b767251fa20838afbd02a07cfd604c6747cbc5
wear <wear63659220@gmail.com>
2009-07-13 15:21:19 +0800

    add parse by crack

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 # Include hook code here
-taobao_config_file = "#{RAILS_ROOT}/config/taobao.yml"  
 
 require 'open_taobao' 
 
+taobao_config_file = "#{RAILS_ROOT}/config/taobao.yml"
 TAOBAO = OpenTaobao.load_configuration(taobao_config_file)
--- lib/open_taobao.rb
+++ lib/open_taobao.rb
@@ -11,7 +11,7 @@ module OpenTaobao
         if defined? RAILS_ENV
           @raw_taobao_configuration = @raw_taobao_configuration[RAILS_ENV]
         end
-       # Thread.current[:fb_api_config] = @raw_taobao_configuration unless Thread.current[:fb_api_config]
+
         apply_configuration(@raw_taobao_configuration)
       end
     end
@@ -24,25 +24,38 @@ module OpenTaobao
     end 
     
     def generate_url(pasted)
-      "http://gw.sandbox.taobao.com/router/rest?" + url_params(pasted)
+      case @taobao_configuration['host']
+      when 'sandbox'
+        "http://gw.sandbox.taobao.com/router/rest?" + url_params(pasted)
+      when 'prod'
+       # something
+     end
     end
     
+    def get_with(joined_params = {})
+       pra = self.pasted_params.merge(joined_params)
+       parse_result RestClient.get(generate_url(pra))       
+    end
     
+    def post_with
+       # post method
+    end
+
     def url_encode(str)
     	return str.gsub!(/[^\w$&\-+.,\/:;=?@]/) { |x| x = format("%%%x", x[0])}
     end 
 
     def url_params(pasted)
-      total_param = pasted.to_a.collect{|key,value| key.to_s+"="+value} + ["sign=#{generate_sign(pasted)}"]
+      total_param = pasted.to_a.collect{|key,value| key.to_s+"="+value.to_s} + ["sign=#{generate_sign(pasted)}"]
       url_encode  total_param.join("&")
     end
 
     def pasted_params
       {
-        :app_key => @taobao_configuration['api_key'].to_s ,
+        :app_key => @taobao_configuration['api_key'],
       #  :method =>'taobao.shop.get',
         :format=> @taobao_configuration['format'], 
-        :v => @taobao_configuration['v'].to_s,
+        :v => @taobao_configuration['v'],
         :timestamp => timestamp,
      #   :fields =>'sid,cid,nick,title,desc,bulletin,pic_path,created,modified',
      #   :nick => 'alipublic05'
@@ -56,6 +69,15 @@ module OpenTaobao
     
     def timestamp
       Time.now.strftime("%Y-%m-%d %H:%M:%S")
+    end 
+    
+    def parse_result(result)
+      case @taobao_configuration['format']
+      when 'xml'  
+        Crack::XML.parse(result)['rsp']
+      when 'json'
+        Crack::JSON.parse(result)
+      end
     end
     
   end     
