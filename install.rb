00e0a668a850391af2f87194785dab263328ba2d
tommyxiao <xiaods@gmail.com>
2009-08-28 12:06:45 +0800

    fixed taobao config file copy route name.xds

--- install.rb
+++ install.rb
@@ -5,6 +5,6 @@ require 'rubygems'
 dir = File.dirname(__FILE__)
 config = File.join('config', 'taobao.yml')   
 
-FileUtils.cp File.join(templates, config), File.join(RAILS_ROOT, config) unless File.exist?(File.join(RAILS_ROOT, config))
+FileUtils.cp File.join('template', config), File.join(RAILS_ROOT, config) unless File.exist?(File.join(RAILS_ROOT, config))
 
 puts IO.read(File.join(dir, 'README'))
