2f7b32c2b5684653c82ef684bfd7ce6107b09ca5
wear <wear63659220@gmail.com>
2009-08-31 16:45:31 +0800

    fix

--- install.rb
+++ install.rb
@@ -1,6 +1,6 @@
 
 dest_file = File.join(RAILS_ROOT, "config/taobao.yml")
-src_file = File.join(File.dirname(__FILE__) , 'template/config/taobao.yml')
+src_file = File.join(File.dirname(__FILE__) , 'temlpate/config/taobao.yml')
 FileUtils.cp_r(src_file, dest_file)
  
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
