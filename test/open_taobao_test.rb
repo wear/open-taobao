339c20d5eb263fc80ebace5ed5ab25605146f4c3
tommyxiao <xiaods@gmail.com>
2009-10-22 17:18:56 +0800

    test unit.xds

--- test/open_taobao_test.rb
+++ test/open_taobao_test.rb
@@ -10,13 +10,15 @@ include OpenTaobao::Shop
 
 
 class OpenTaobaoTest < Test::Unit::TestCase
-  
 
   def test_sellercats
     result=OpenTaobao.get_with(:method =>'taobao.sellercats.list.get',:nick => 'alipublic23')
     puts result
   end
-
-
-
+  
+  def test_init
+    
+  end
+  
+  
 end
